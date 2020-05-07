e9a2bceddc4091c6e79c28d39246ac2f70734aa4
Mohammad Beheshti <mbeheshtis@gmail.com>
2020-05-07 23:54:59 +0430

    basic webpack config

---
+++ config/postcss.config.js
@@ -0,0 +1,41 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: [
+    './**/*.html',
+    // etc.
+  ],
+
+  // This is the function used to extract class names from your templates
+  defaultExtractor: content => {
+    // Capture as liberally as possible, including things like `h-(screen-1.5)`
+    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+
+    // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+
+    return broadMatches.concat(innerMatches);
+  },
+});
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({
+      stage: 2,
+      features: {
+        'nesting-rules': true,
+        'prefers-color-scheme-query': true,
+      },
+      autoprefixer: {
+        flexbox: 'no-2009',
+        grid: 'autoplace',
+      },
+    }),
+    purgecss,
+    // ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
+};
