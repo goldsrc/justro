f83f291f35acaed8ad1d32af64eb5885eb10ed39
Mohammad Beheshti <mbeheshtis@gmail.com>
2020-05-08 16:09:02 +0430

    finished webpack boiler plate (added main.scss, lazy image loading)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
 import './css/main.css';
+import './scss/main.scss';
+
+const lazyImages = [...document.querySelectorAll('img.lazy')];
+
+const lazyImageObserver = new IntersectionObserver((entries) => {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      const lazyImage = entry.target as HTMLImageElement;
+      lazyImage.src = lazyImage.dataset.src || '';
+      lazyImage.srcset = lazyImage.dataset.srcset || '';
+      lazyImage.classList.remove('lazy');
+      lazyImageObserver.unobserve(lazyImage);
+    }
+  });
+});
+
+lazyImages.forEach(function (lazyImage) {
+  lazyImageObserver.observe(lazyImage);
+});
