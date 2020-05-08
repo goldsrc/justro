584e11d9d233c3ec696313f8e0265c7722206bb4
Mohammad Beheshti <mbeheshtis@gmail.com>
2020-05-08 21:50:12 +0430

    better purgecss config

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,18 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable global-require */
 const purgecss = require('@fullhuman/postcss-purgecss')({
-  // Specify the paths to all of the template files in your project
   content: [
     './**/*.html',
-    // etc.
   ],
 
-  // This is the function used to extract class names from your templates
   defaultExtractor: content => {
-    // Capture as liberally as possible, including things like `h-(screen-1.5)`
     const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
 
-    // Capture classes within other delimiters like .block(class="w-1/2") in Pug
     const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
 
     return broadMatches.concat(innerMatches);
@@ -21,7 +16,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
 module.exports = {
   plugins: [
-    require('tailwindcss')({ config: './src/css/tailwind.config.js' }),
+    require('tailwindcss')({ config: './config/tailwind.config.js' }),
     require('autoprefixer'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
@@ -35,7 +30,7 @@ module.exports = {
         grid: 'autoplace',
       },
     }),
-    purgecss,
-    // ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+    //purgecss,
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
   ],
 };
